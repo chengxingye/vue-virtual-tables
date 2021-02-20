819762bb5e27b4bbfd6a1bec743463846cb5c531
chengxingye <chengxingye@kuaishou.com>
2021-02-20 18:11:03 +0800

    修改

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+function resolveSrc(_path) {
+  return path.join(__dirname, _path);
+}
+module.exports = {
+  resolve: {
+    alias: {
+      assets: resolveSrc('./example')
+    }
+  },
+}
